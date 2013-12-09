572cf80d904bb86091c389c2b8ff1ea7fb25ef7d
hezzze <hezzze@gmail.com>
2013-12-09 01:11:07 -0500

    Committed the first prototype of the music app Melodothogy. Current features: 1. Use of jQuery Mobile and CreateJS for the UI and visualization 2. Incorporate Box2D for the visual effects 3. Two Modes: Random playground and Keyboard
    Features to come:
    1. Play songs according to the number of fingers touched
    2. Generate random tones 
    3. shake mode

---
+++ Methodology/Synth.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+(function(window) {
+
+	function Synth(context) {
+		this.context = context;
+		this.isPlaying = false;
+
+		try {
+			var vco = context.createOscillator();
+			vco.type = vco.SINE;
+
+			var vca = context.createGain();
+			vca.gain.value = 0;
+
+			vco.connect(vca);
+			vca.connect(context.destination);
+
+			// this.vco = vco;
+			// this.vca = vca;
+
+			this.play = function(frequency) {
+				vco.frequency.value = frequency;
+				vco.start(0);
+				vca.gain.value = 0.3;
+				this.isPlaying = true;
+			};
+
+			this.stop = function() {
+				vca.gain.value = 0;
+				this.isPlaying = false;
+			};
+
+		} catch (err) {
+			alert(err.message);
+		}
+
+	}
+
+	window.Synth = Synth;
+
+})(window);
